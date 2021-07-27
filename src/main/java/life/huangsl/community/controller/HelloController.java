4d7b52a1166f8385e8846ab68d8d5d46e0398b97
hsl <huangsl@bupt.edu.cn>
2021-07-27 22:00:46 +0800

    init repo

---
+++ src/main/java/life/huangsl/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package life.huangsl.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author hsl
+ * @create 2021-07-27 21:37
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
